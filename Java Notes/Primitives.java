d1572232e9498bfa0ff8c555f503a5e8aa4a5585
TwcL774 <tristanwright318@revature.net>
2022-12-06 12:22:51 -0500

    added primitives definition

---
+++ Java Notes/Primitives.java
@@ -0,0 +1,22 @@
+public class Primitives {
+    /*
+     * Java represents the most basic of data in structures called primitives: these data types are the simpliest
+     */
+
+    // these primitives represents whole numbers
+    byte b = 0; // 8 bit or 1 Byte
+    short s = 0; // 16 bits or 2 Bytes
+    int i = 0; // 34 bits or 4 Bytes
+    long l = 0; // 64 bits or 8 Bytes
+
+    // these primitives represents decimals
+    float f = 0f; // 32 bits or 4 Bytes
+    double d = 0.0; // 64 bits or 8 Bytes
+
+    // this primitive represents the unicode character
+    char c = 'c'; // 16 bits or 2 Bytes
+
+    // this primitive represents logical true or false
+    boolean bool = true; // 1 bit
+
+}
