51b4b0fbac1a16d6aa94874eb56142b2a74b02f0
TwcL774 <tristanwright318@revature.net>
2022-12-06 14:38:58 -0500

    added naming convention example

---
+++ Java Notes/NamingConventions.java
@@ -0,0 +1,13 @@
+public class NamingConventions {
+    /*
+     * All Classes utilize PascalCase Syntax
+     *  - All unique words in the name start with capital
+     *  - does not start with a number
+     * 
+     * All Variables (reference to data) utilize camelCase Syntax
+     *  - First unique world is all lowercase, 2nd and other unique word starts with capital letter
+     *  - does not start with a number
+     * 
+     * Any Constant values shoulb be written in SCREAMING_SNAKE case
+     */
+}
