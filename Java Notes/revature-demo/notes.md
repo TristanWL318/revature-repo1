105d39a1e41253089a4e62ccf1ee89fe8f105512
TwcL774 <tristanwright318@revature.net>
2022-12-08 13:07:22 -0800

    Hello World! web app

---
+++ Java Notes/revature-demo/notes.md
@@ -0,0 +1,12 @@
+# POM stand for Project Object Model
+    POM tells Maven relevant information for the Project.
+        - Properties
+            tells Maven what Java version to use
+        - Dependencies
+            tells Maven 3rd Party Libraries
+        - Build
+            plugins are added to the Project
+
+
+# All Maven Projects have their code stored inside of a src/main/java directory
+    
--- Java Notes/revature-demo/src/main/java/com/example/App.java
+++ Java Notes/revature-demo/src/main/java/com/example/App.java
@@ -1,13 +1,22 @@
 package com.example;
 
+import io.javalin.Javalin;
+
 /**
- * Hello world!
+ * Hello world app
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+
+        // Creates a new instance w/out config
+        Javalin testApp = Javalin.create();
+
+        testApp.get("/hello", ctx -> ctx.result("Hello World!"));
+
+        testApp.start();
     }
 }
